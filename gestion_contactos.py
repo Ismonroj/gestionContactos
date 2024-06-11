5f3e98490bb5f21f6b589beb557dac0698e6e684
ismonroj <ismaelmontero.r@gmail.com>
2024-06-11 09:05:02 +0200

    Se agregó 'eliminar'

--- gestion_contactos.py
+++ gestion_contactos.py
@@ -9,10 +9,14 @@ def listar_contactos():
     for contacto in contactos:
         print(f"Nombre: {contacto['nombre']}, Teléfono: {contacto['telefono']}, Email: {contacto['email']}")
 
+def eliminarContactos(nombre):
+    global contactos
+    contactos = [contacto for contacto in contactos if contacto["nombre"] != nombre]
+
 def main():
     print("Aplicación de Gestión de Contactos")
     while True:
-        comando = input("Comando (agregar/listar/salir): ")
+        comando = input("Comando (agregar/listar/eliminar/salir): ")
         if comando == "agregar":
             nombre = input("Nombre: ")
             telefono = input("Teléfono: ")
@@ -20,6 +24,9 @@ def main():
             agregar_contacto(nombre, telefono, email)
         elif comando == "listar":
             listar_contactos()
+        elif comando == "eliminar":
+            nombre = input("Nombre del contacto a eliminar: ")
+            eliminarContactos(nombre)
         elif comando == "salir":
             break
 
