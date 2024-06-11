d1091a4f0ac7ecee84bc19572f072be0a9997a7c
ismonroj <ismaelmontero.r@gmail.com>
2024-06-11 16:18:46 +0200

    Se agregó la función Buscar

--- gestion_contactos.py
+++ gestion_contactos.py
@@ -13,10 +13,21 @@ def eliminarContactos(nombre):
     global contactos
     contactos = [contacto for contacto in contactos if contacto["nombre"] != nombre]
 
+def buscarContacto(nombre):
+    encontrado = False
+    for contacto in contactos:
+        if contacto['nombre'] == nombre:
+            print(f"Nombre: {contacto['nombre']}, Teléfono: {contacto['telefono']}, Email: {contacto['email']}")
+            encontrado = True
+            break
+    if not encontrado:
+        print("Contacto no encontrado")
+    
+
 def main():
     print("Aplicación de Gestión de Contactos")
     while True:
-        comando = input("Comando (agregar/listar/eliminar/salir): ")
+        comando = input("Comando (agregar/listar/eliminar/salir/buscar): ")
         if comando == "agregar":
             nombre = input("Nombre: ")
             telefono = input("Teléfono: ")
@@ -27,6 +38,9 @@ def main():
         elif comando == "eliminar":
             nombre = input("Nombre del contacto a eliminar: ")
             eliminarContactos(nombre)
+        elif comando == "buscar":
+            nombre = input("Nombre del contacto a buscar: ")
+            buscarContacto(nombre)
         elif comando == "salir":
             break
 
